40b9a8f2aa7ba4898b0026867c3888b15939313f
vikasbhat89 <51418626+vikasbhat89@users.noreply.github.com>
2020-04-27 18:08:02 +0530

    modelclass

---
+++ PersonDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NumberToWordAPI.Models
+{
+    public class PersonDetail
+    {
+        public string name { get; set; }
+        public decimal number { get; set; }
+    }
+}
